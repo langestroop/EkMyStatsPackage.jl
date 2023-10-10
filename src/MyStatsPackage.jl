9b4008bd90c0f68e0ddc235e8d0608025b25268a
emanuel-kopp <emanuel.kopp@outlook.com>
2023-10-10 10:07:49 +0200

    Added basic structure and files for my new package

---
+++ src/MyStatsPackage.jl
@@ -0,0 +1,6 @@
+module MyStatsPackage
+
+include("statistic_functions.jl")
+export rse_mean, rse_std, rse_sum, StatResult2
+
+end # module MyStatsPackage
---
+++ src/statistic_functions.jl
@@ -0,0 +1,56 @@
+#---
+function rse_sum(x)
+    +(x...)
+end
+
+rse_sum(1:36) == 666
+
+#---
+function rse_mean(x)
+    rse_sum(x)/length(x)
+end
+
+rse_mean(-15:17) == 1
+
+#---
+function rse_std(x)
+    sqrt(sum((x.-rse_mean(x)).^2) /(length(x)-1))
+end
+
+rse_std(1:3)
+
+#---
+function rse_tstat(x; σ = rse_std(x))
+    rse_mean(x)/ (σ / sqrt(length(x)))
+end
+
+rse_tstat(2:3)
+rse_tstat(2:3, σ = 4)
+
+# ---
+
+struct StatResult2
+    x::Vector
+    n::Int64
+    std::Float64
+    tvalue::Float64
+end
+
+function  StatResult2(x)
+    n = length(x)
+    std = rse_std(x)
+    tvalue = rse_tstat(x)
+    return StatResult2(x, n, std, tvalue)
+end
+
+StatResult2(2:10)
+
+import Base.length
+function length(x::StatResult2)
+    return x.n
+end
+
+length(StatResult2(2:15))
+
+#---
+
